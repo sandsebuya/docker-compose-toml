08e06b6d50327e6792ff7dc3b28628558e2e1fd4
sandsebuya <oleg@palkas.ru>
2025-09-27 13:12:04 +0300

    Firts

---
+++ README.md
@@ -0,0 +1,13 @@
+# Building
+Create venv, install all packages from req.txt
+
+Build it
+
+
+``
+pyinstaller --onefile main.py
+``
+
+If you want to install it system-wide just put /dist/main to /bin directory
+
+``sudo mv dist/main /bin/docker-compose-toml``
---
+++ main.py
@@ -0,0 +1,39 @@
+import tomllib
+import yaml
+import sys
+import subprocess
+
+
+def generate_yaml(data: dict):
+    if len(sys.argv) == 1:
+        yaml_str = yaml.dump(data, allow_unicode=True, default_flow_style=False)
+        print(yaml_str)
+    with open("docker-compose.yaml", "w", encoding="utf-8") as f:
+        yaml.dump(data, f, allow_unicode=True, default_flow_style=False)
+
+
+def run_compose():
+    if len(sys.argv) == 1:
+        raise SystemExit()
+    try:
+        sys_arg = sys.argv[1:]
+        sys_arg.insert(0, "docker-compose")
+        process = subprocess.Popen(
+            sys_arg, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        )
+
+        for line in process.stdout:
+            print(line, end="")
+
+        process.wait()
+        print(f"Команда завершилась с кодом: {process.returncode}")
+    except KeyboardInterrupt:
+        print("Pressed Ctrl+C, killing process...")
+        process.terminate()
+        process.wait()
+
+
+if __name__ == "__main__":
+    parsed_toml = read_toml()
+    generate_yaml(parsed_toml)
+    run_compose()
